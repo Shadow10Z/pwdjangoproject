cf5976d3f53bf70e97e1a54ad058bba76eb3fd61
João Serralha <jfserralha@hotmail.com>
2023-06-13 16:21:25 +0100

    changes CSS file

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Artigo(models.Model):
+    titulo = models.CharField(max_length=200)
+    prioridade = models.IntegerField(default=1)
+    concluido = models.BooleanField(default=False)
+    criado = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.titulo
